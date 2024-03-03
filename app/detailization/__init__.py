1465b8fa7226d75793b24aa1ba1c7a7479fbc4d3
Ilya Lyaukin <kzerby@gmail.com>
2024-03-03 11:59:03 -0600

    Add gender detailizer

--- app/detailization/__init__.py
+++ app/detailization/__init__.py
@@ -4,4 +4,5 @@ from .datetime_detailizer import DatetimeDetailizer
 from .number_detailizer import NumberDetailizer
 from .money_detailizer import MoneyDetailizer
 from .geo_detailizer import GeoDetailizer
+from .gender_detailizer import GenderDetailizer
 from .get_details_for_cells import get_details_for_cells, call_get_details_for_all_cols
